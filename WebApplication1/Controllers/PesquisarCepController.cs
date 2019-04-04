adb29e5581f96d6d338203224f5f97fa7d8d8710
cesar augusto gadelha <cesar_ags@outlook.com>
2019-04-04 14:26:15 -0300

    inicial

---
+++ WebApplication1/Controllers/PesquisarCepController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestSharp;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class PesquisarCepController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult MostraResultado(int cep)
+        {
+            
+                var resquest = new RestClient("http://www.viacep.com.br/ws/" + cep + "/json");
+                var request = new RestRequest(Method.GET);
+                IRestResponse response = resquest.Execute(request);
+                var data = Newtonsoft.Json.JsonConvert.DeserializeObject<Dados>(response.Content);
+                return View(data);
+            
+        }
+
+
+    }
+}
